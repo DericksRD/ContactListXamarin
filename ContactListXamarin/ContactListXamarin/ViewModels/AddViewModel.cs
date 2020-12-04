04632af1776aa0b289626e70ba8c6d40becc3375
Derick <derick290801@gmail.com>
2020-12-04 17:31:11 -0400

    Add page completed

---
+++ ContactListXamarin/ContactListXamarin/ViewModels/AddViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ContactListXamarin.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ContactListXamarin.ViewModels
+{
+    class AddViewModel
+    {
+        public String Name { get; set; }
+        public String Phone { get; set; }
+        public Contact NewContact => new Contact(Name, Phone);
+
+        public ICommand AddCommand => new Command(AddButton);
+
+        public async void AddButton()
+        {
+            ListViewModel viewModel = new ListViewModel();
+            viewModel.Add(NewContact);
+            await App.Current.MainPage.DisplayAlert(
+                "Usuario Agregado exitosamente!", "",
+                "Ok");
+            await App.Current.MainPage.Navigation.PopModalAsync();
+        }
+        
+    }
+}
--- ContactListXamarin/ContactListXamarin/ViewModels/ListViewModel.cs
+++ ContactListXamarin/ContactListXamarin/ViewModels/ListViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using ContactListXamarin.Models;
+using ContactListXamarin.Views;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace ContactListXamarin.ViewModels
 {
@@ -15,6 +18,18 @@ namespace ContactListXamarin.ViewModels
             {
                 new Contact("Erick Sánchez", "829-439-7453")
             };
+        }
+
+        public ICommand AddCommand => new Command(AddClicked);
+
+        private async static void AddClicked(object obj)
+        {
+            await App.Current.MainPage.Navigation.PushModalAsync(new AddPage());
+            //throw new NotImplementedException();
+        }
+        public void Add(Contact newContact)
+        {
+            ContactList.Add(newContact);
         }
     }
 }
